04248524568de0530447a82199a5fd4c448f20e4
Siddharth <siddhartha.marko.3@gmail.com>
2024-02-04 22:43:53 +0530

    card by pure css update

--- src/component/checkedList.jsx
+++ src/component/checkedList.jsx
@@ -1,18 +1,41 @@
 import React from "react"
 import TaskCard from "./taskCard"
 
-    export default function CheckedList({data}){
-        // state use for pop up create name of list
-        function rename() { }
+const options = [
+    "Sort by date",
+    "Sort by My order",
+    "Rename List",
+    "Delete List",
+    "Delete all complete tasks"
+];
+export default function CheckedList({ data }) {
 
-        const dataInList = data.task.map((item) => {
-        return <TaskCard key={item.id} data={item}/>
-    })   
-    // console.log(data);
-    return(
-        <div className="list" > 
-            <h1>{data.name}</h1>
-            {dataInList}
+
+    const dataInList = data.task.map((item) => {
+        return <TaskCard key={item.id} data={item} />
+    })
+    
+    return (
+        <div className="card" id="style-3">
+            <div className="head">
+                <div>
+                    <h1>{data.name}</h1>
+                </div>
+                <div className="dots">
+                    <div className="dot-container">
+                        <i>...</i>
+                        <ul className="dot-options d-none ">
+                            {options}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="task">
+                <ul className="task-list">
+                {dataInList}
+
+                </ul>
+            </div>
         </div>
     )
 }
--- src/component/taskCard.jsx
+++ src/component/taskCard.jsx
@@ -1,7 +1,11 @@
 import React from "react"
 import { useData } from "./context/task"
 
-
+const Options = [
+    "Delete",
+    "Add Different List",
+    "New List"
+];
 export default function TaskCard({data}){
     const {state, dispatcher} = useData();
 
@@ -11,6 +15,10 @@ export default function TaskCard({data}){
             type: 'delete',
             id: 'list1',
             taskId: data.id,
+        },
+        sort: {
+            type: 'sort',
+            id: 'list1',
         },
         move: {
             type: 'move',
@@ -25,13 +33,26 @@ export default function TaskCard({data}){
     }
     // console.log(data);
     return ( <>
-            <div className="tasks">
-                <p>{data.title}</p>
-                <button onClick={() => dispatcher(op.rename)} >rename task</button>
-                <button onClick={() => dispatcher(op.delete)} >delete task</button>
-                <button onClick={() =>dispatcher(op.move)} >move list</button>
-                <button onClick={() => dispatcher(op.complete)}>Delete complete</button>
-                {/* <button>sort by</button> */}
+        <li className="addTask">Add Task</li>
+        <li>
+            <input type="checkbox" />
+            <p>Task list 1</p>
+            <div className="dot-container">
+                <i>...</i>
+                <ul className="dot-options d-none ">
+                    {Options}
+                    </ul>
             </div>
+        </li>
+      
         </>)
 }
+{/* <div className="tasks">
+    <span>{data.title}</span>
+    <button onClick={() => dispatcher(op.sort)} >Sort</button>
+    <button onClick={() => dispatcher(op.rename)} >rename task</button>
+    <button onClick={() => dispatcher(op.delete)} >delete task</button>
+    <button onClick={() => dispatcher(op.move)} >move list</button>
+    <button onClick={() => dispatcher(op.complete)}>Delete complete</button>
+    <button>sort by</button>
+</div> */}
