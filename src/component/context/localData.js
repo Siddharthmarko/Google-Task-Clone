bdd6bf6d122805ede6f4863f8b4c157246828d0b
Siddharth <siddhartha.marko.3@gmail.com>
2024-02-15 23:21:09 +0530

    remove extra demo data

--- src/component/context/localData.js
+++ src/component/context/localData.js
@@ -19,38 +19,7 @@ let defaultList = [
             },
         ]
     },
-    {
-        "name": "Work Tasks",
-        "id": "2b6c9d3d-98f7-4e90-a52e-9e87179c4dbd",
-        "checked": true,
-        "task": [
-            {
-                "title": "Cleaning",
-                "id": 3,
-                "listId": "2b6c9d3d-98f7-4e90-a52e-9e87179c4dbd",
-                "complete": false
-            }
-        ]
-    },
-    {
-        "name": "Personal Goals",
-        "id": "f3b7a8fe-65d2-41c5-b5a1-82a15b73a639",
-        "checked": false,
-        "task": []
-    },
-    {
-        "name": "Fitness Routine",
-        "id": "9e4bb6b8-144f-45d7-8eaf-6ef0dd33a462",
-        "checked": false,
-        "task": [
-            {
-                "title": "leg Day",
-                "id": 4,
-                "listId": "9e4bb6b8-144f-45d7-8eaf-6ef0dd33a462",
-                "complete": false
-            }
-        ]
-    }
+    
 ];
 
 
